5cade351dd5692a58d044e68af1e6fc6ca942a83
NikitaBes4 <mandarin215@gmail.com>
2024-02-01 08:31:45 +0000

    01.02

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,7 @@
-<script>import "../app.pcss";</script><slot></slot>
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+</nav>
+
+<slot></slot>
+<script>import "../app.pcss";</script>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,2 @@
+<p>Пиши че хочешь</p>
+<input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
