9209dda67ed1e6837324201e3ecc727366e3235f
NikitaBes4 <mandarin215@gmail.com>
2024-02-14 06:45:38 +0000

    14.02

--- src/lib/py/pydata.py
+++ src/lib/py/pydata.py
@@ -3,15 +3,16 @@ import requests
 import pandas as pd
 
 
-def getcountries():
+def multiple(a, b):
    url="https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
    df=pd.read_csv(url)
 
    return df.to_numpy()  #np.multiply(a, b).tolist()
 
-def regions():
+def getcountries():
    url="https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
+   
    df=pd.read_csv(url)
    regions = df.Region.unique()
 
-   return {"alldata":df.to_numpy(), "region":regions}
+   return {"alldata":df.to_numpy(), "regions":regions}
--- src/routes/countries/[cont]/+page.svelte
+++ src/routes/countries/[cont]/+page.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <svelte:head>
-	<title>{$page.params.count}</title>
+	<title>{$page.params.cont}</title>
 </svelte:head>
 
 <div style="border:1px solid blue;padding:25px">
-	{#each data.world.alldata.filter((country) => country[1] == $page.params.count.toUpperCase()) as item}
+	{#each data.world.alldata.filter((country) => country[1] == $page.params.cont.toUpperCase()) as item}
 		<p>{item[0]}</p>
 	{/each}
 </div>
