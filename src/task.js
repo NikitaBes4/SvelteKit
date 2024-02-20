1cd343ab7316f14eb9d5d5446e8834bc51816e8d
NikitaBes4 <mandarin215@gmail.com>
2024-02-20 06:49:11 +0000

    20.02

---
+++ src/task.js
@@ -0,0 +1,11 @@
+let arr = ["abba","hello","home"]
+function pfilter(x)
+{
+    if((x == x.split("").reverse().join("")))
+    return x
+}
+let pol = arr.filter(pfilter)
+console.log(pol)
+
+let num = [12.55555, 13.66666, 14.77777]
+
